b75bf76854c7754c6e6f3083cd68b412c9631069
skkim125 <kthanks125@gmail.com>
2024-05-24 18:54:52 +0900

    여행 프로젝트 - 메거진 화면 TableView Cell 오토레이아웃 설정 / dateFormat 함수로 custom / 라벨 UI 설정 / imageView backgroundClolor로 임시 설정

---
+++ Assignment_240524/MagazineTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MagazineTableViewCell.swift
+//  Assignment_240524
+//
+//  Created by 김상규 on 5/24/24.
+//
+
+import UIKit
+
+class MagazineTableViewCell: UITableViewCell {
+
+    @IBOutlet var travelImageView: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var subTitleLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+}
---
+++ Assignment_240524/MagazineTableViewController.swift
@@ -0,0 +1,59 @@
+//
+//  MagazineTableViewController.swift
+//  Assignment_240524
+//
+//  Created by 김상규 on 5/24/24.
+//
+
+import UIKit
+
+class MagazineTableViewController: UITableViewController {
+
+    let magazine = MagazineInfo().magazine
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.rowHeight = 480
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return magazine.count
+    }
+ 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MagazineTableViewCell") as! MagazineTableViewCell
+        
+        cell.travelImageView.image = UIImage(named: magazine[indexPath.row].photo_image)
+        cell.travelImageView.layer.cornerRadius = 10
+        
+        cell.titleLabel.text = magazine[indexPath.row].title
+        cell.titleLabel.font = .boldSystemFont(ofSize: 20)
+        cell.titleLabel.numberOfLines = 2
+        
+        cell.subTitleLabel.text = magazine[indexPath.row].subtitle
+        cell.subTitleLabel.font = .boldSystemFont(ofSize: 16)
+        cell.subTitleLabel.textColor = .gray
+        cell.subTitleLabel.numberOfLines = 2
+        
+        cell.dateLabel.text = dateFormat(magazine[indexPath.row].date)
+        cell.dateLabel.font = .boldSystemFont(ofSize: 14)
+        cell.dateLabel.textColor = .lightGray
+        cell.dateLabel.textAlignment = .right
+        
+        return cell
+    }
+    
+    func dateFormat(_ date: String) -> String {
+        let dateStr = "230125"
+        let dateFormat = DateFormatter()
+        dateFormat.dateFormat = "yyMMdd" // DateFormatter()의 dateFormat 설정
+        let convertDate = dateFormat.date(from: dateStr) // Date 타입으로 변환
+
+        let myDateFormatter = DateFormatter()
+        myDateFormatter.dateFormat = "yy년 MM월 dd일"
+
+        return myDateFormatter.string(from: convertDate!)
+    }
+}
