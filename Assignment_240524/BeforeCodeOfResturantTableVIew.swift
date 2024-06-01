bad9fe2e818f58d7c65c88a1123aba1e8067660e
skkim125 <kthanks125@gmail.com>
2024-06-01 23:13:40 +0900

    트래블톡: 톡방 클릭 시 마지막 셀로 이동하게 설정(방법1. viewDidLayoutSubviews, 방법2. 비동기 작업)

--- Assignment_240524/BeforeCodeOfResturantTableVIew.swift
+++ Assignment_240524/BeforeCodeOfResturantTableVIew.swift
@@ -5,7 +5,7 @@
 //  Created by 김상규 on 5/25/24.
 //
 
-import Foundation
+// import Foundation
 
 //        let imageUrl = URL(string: list[indexPath.row].image)
 //        let placeholder = UIImage(systemName: "arrow.clockwise.circle")
@@ -49,3 +49,71 @@ import Foundation
 //        let likeImg = list[indexPath.row].like ? "heart.fill" : "heart"
 //        cell.likeButton.setImage(UIImage(systemName: likeImg), for: .normal)
 //        cell.likeButton.tintColor = .red
+
+
+//func detailTableViewSetting() {
+//    chattingDetailTableView.delegate = self
+//    chattingDetailTableView.dataSource = self
+//    chattingDetailTableView.rowHeight = UITableView.automaticDimension
+//    chattingDetailTableView.separatorStyle = .none
+//    
+//    let userChatXib = UINib(nibName: UserChatTableViewCell.identifier, bundle: nil)
+//    chattingDetailTableView.register(userChatXib, forCellReuseIdentifier: UserChatTableViewCell.identifier)
+//    
+//    let myChatXib = UINib(nibName: MyChatTableViewCell.identifier, bundle: nil)
+//    chattingDetailTableView.register(myChatXib, forCellReuseIdentifier: MyChatTableViewCell.identifier)
+//    
+//    // 방법 2) 비동기 작업
+//    DispatchQueue.main.async {
+//        self.setScrollBottom()
+//        print("=======")
+//    }
+//}
+//
+//
+//override func viewWillAppear(_ animated: Bool) {
+//    print(#function)
+//}
+//
+//override func loadViewIfNeeded() {
+//    super.loadViewIfNeeded()
+//    print(#function)
+//    
+//}
+//
+//override func viewIsAppearing(_ animated: Bool) {
+//    print(#function)
+//}
+//
+//override func viewWillLayoutSubviews() {
+//    print(#function)
+//}
+//
+//// 방법 1) 뷰가 생성되어 화면에 보이기 전 작업을 진행
+//// 뷰가 생성되고 화면에 보이기 직전 https://ios-daniel-yang.tistory.com/93
+//// "UIVIew" 객체가 모든 서브뷰들의 레이아웃을 변경한 후에 호출되는 메서드
+//override func viewDidLayoutSubviews() {
+//    super.viewDidLayoutSubviews()
+//    print(#function)
+//    
+////        setScrollBottom()
+//}
+//
+//override func viewDidAppear(_ animated: Bool) {
+//    print(#function)
+//}
+//
+//override func viewWillDisappear(_ animated: Bool) {
+//    print(#function)
+//}
+//
+//override func viewDidDisappear(_ animated: Bool) {
+//    print(#function)
+//}
+//
+//func setScrollBottom() {
+//    guard let chats = chatList else { return }
+//    let endIndex = IndexPath(row: chats.count - 1, section: 0)
+//    
+//    chattingDetailTableView.scrollToRow(at: endIndex, at: .bottom, animated: false)
+//}
--- Assignment_240524/Chatting/ChattingDetailViewController.swift
+++ Assignment_240524/Chatting/ChattingDetailViewController.swift
@@ -45,6 +45,27 @@ class ChattingDetailViewController: UIViewController {
         
         let myChatXib = UINib(nibName: MyChatTableViewCell.identifier, bundle: nil)
         chattingDetailTableView.register(myChatXib, forCellReuseIdentifier: MyChatTableViewCell.identifier)
+        
+        // 방법 2) 비동기 작업
+        //DispatchQueue.main.async {
+        //    self.setScrollBottom()
+        //}
+    }
+    
+    // 방법 1) 뷰가 생성되어 화면에 보이기 전 작업을 진행
+    // 뷰가 생성되고 화면에 보이기 직전 https://ios-daniel-yang.tistory.com/93
+    // "UIVIew" 객체가 모든 서브뷰들의 레이아웃을 변경한 후에 호출되는 메서드
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        setScrollBottom()
+    }
+    
+    func setScrollBottom() {
+        guard let chats = chatList else { return }
+        let endIndex = IndexPath(row: chats.count - 1, section: 0)
+        
+        chattingDetailTableView.scrollToRow(at: endIndex, at: .bottom, animated: false)
     }
 }
 
@@ -65,6 +86,7 @@ extension ChattingDetailViewController: UITableViewDelegate, UITableViewDataSour
         let myChatDetailCell = tableView.dequeueReusableCell(withIdentifier: MyChatTableViewCell.identifier, for: indexPath) as! MyChatTableViewCell
         
         if let data = chatList {
+            
             if data[indexPath.row].user == .user {
                 myChatDetailCell.myChatTableViewCell(data: data[indexPath.row])
                 return myChatDetailCell
@@ -78,5 +100,13 @@ extension ChattingDetailViewController: UITableViewDelegate, UITableViewDataSour
         return userChatDetailCell
     }
     
-    
+    // tableView가 화면에 나타기 전에 실행되는 메서드
+    // 스크롤이 되지 않음...
+//    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+//        guard let chats = chatList else { return }
+//        let endIndex = IndexPath(row: chats.endIndex-1, section: 0)
+//        
+//        print(indexPath.row)
+//        tableView.scrollToRow(at: endIndex, at: .bottom, animated: false)
+//    }
 }
